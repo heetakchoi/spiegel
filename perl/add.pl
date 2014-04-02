dcf8e7ed94bc566b72f93d485c69d61a56a9d109
heetakchoi <heetak.choi@campmobile.com>
2014-04-02 21:30:49 +0900

    예제 수정

--- perl/add.pl
+++ perl/add.pl
@@ -12,8 +12,8 @@ sub t_Math;
 sub t_Net;
 sub t_Str;
 
+t_Net;
 t_Math;
-# t_Net;
 t_Str;
 
 sub t_Math{
@@ -30,7 +30,7 @@ sub t_Math{
 sub t_Net{
     my %header_hash = ();
     print send_get("endofhope.com", 80, "/", \%header_hash, 0);
-    print send_ssl_get("play.google.com", 443, "/store/apps/details?id=com.beatpacking.beat", \%header_hash, 1);
+    print send_ssl_get("play.google.com", 443, "/store/apps/collection/topselling_free", \%header_hash, 1);
 }
 
 sub t_Str{
