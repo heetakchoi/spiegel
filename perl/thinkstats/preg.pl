1bf505e11f2128d706de793e52e3d73b6c00d838
heetakchoi <heetak.choi@campmobile.com>
2014-04-09 22:02:03 +0900

    perl oop style 추가

--- perl/thinkstats/preg.pl
+++ perl/thinkstats/preg.pl
@@ -3,6 +3,28 @@
 use strict;
 use warnings;
 
-open(my $fh_r, "<", "2002FemPreg.dat");
-close($fh_r);
+{
+    package FemPreg;
 
+    sub new{
+	my ($class) = @_;
+	my $self = {};
+	my $size = 0;
+	open(my $fh_r, "<", "2002FemPreg.dat");
+	while(<$fh_r>){
+	    $size ++;
+	}
+	close($fh_r);	
+	${$self}{"size"} = $size;
+	bless($self, $class);
+	return $self;
+    }
+
+    sub get_size{
+	my $self = shift;
+	return ${$self}{"size"};
+    }
+}
+
+my $femPreg = FemPreg->new();
+print "FemPreg.size: ". $femPreg->get_size() ."\n";
