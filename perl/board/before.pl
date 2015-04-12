37d50c21afb6d9a0d397b37e3bfef0f2805abf94
heetakchoi <endofhope@naver.com>
2015-04-12 09:42:51 +0900

    광고 시스템 제거

--- perl/board/before.pl
+++ perl/board/before.pl
@@ -11,16 +11,6 @@ sub check;
 
 my $cgi = CGI->new();
 
-print "<script>\n";
-print "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n";
-print "	 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n";
-print "	 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n";
-print "	 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n";
-print "\n";
-print "ga('create', 'UA-61757724-1', 'auto');\n";
-print "ga('send', 'pageview');\n";
-print "</script>\n";
-
 print  "<div class=\"wrap\">\n";
 print  "  <div class=\"head\">\n";
 print  "    <span style=\"font-size:2.0em;font-weight:bold;\">Life Logging</span>\n";
