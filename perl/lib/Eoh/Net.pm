62ae4a59443a639271453aa400230ee5e2c57df7
heetak.choi <heetak.choi@campmobile.com>
2015-04-23 17:56:26 +0900

    header 의 host 값에 port 까지 기본으로 붙이도록 수정

--- perl/lib/Eoh/Net.pm
+++ perl/lib/Eoh/Net.pm
@@ -21,32 +21,32 @@ sub send_ssl_get{
     my ($class_or_self, $host, $port, $req_uri, $ref_headermap, $brief_flag) = @_;
     my $response = "";
     my $socket = IO::Socket::SSL->new(
-		PeerHost => "$host:$port",
-		SSL_verify_mode => 0x02,
-		SSL_ca_file => Mozilla::CA::SSL_ca_file(),
-		)
-		|| die "Can't connect: $@";
+	PeerHost => "$host:$port",
+	SSL_verify_mode => 0x02,
+	SSL_ca_file => Mozilla::CA::SSL_ca_file(),
+	)
+	|| die "Can't connect: $@";
 
     $socket->verify_hostname($host, "http")
-		|| die "hostname verification failure";
+	|| die "hostname verification failure";
 
     my %headermap = %{$ref_headermap};
     unless(defined($headermap{"Host"})){
-		$headermap{"Host"} = $host;
+	$headermap{"Host"} = sprintf("%s:%d", $host, $port);
     }
     unless(defined($headermap{"Connection"})){
-		$headermap{"Connection"} = "close";
+	$headermap{"Connection"} = "close";
     }
 
     print $socket "GET $req_uri HTTP/1.1\r\n";
 
     foreach my $headerkey (keys %headermap){
-		print $socket "$headerkey: $headermap{$headerkey}\r\n";
+	print $socket "$headerkey: $headermap{$headerkey}\r\n";
     }
     print $socket "\r\n";
 
     while(my $line=<$socket>){
-		$response = $response . $line;
+	$response = $response . $line;
     }
 
     shutdown($socket, 2);
@@ -58,21 +58,21 @@ sub send_ssl_get{
 
     my @response_headers = split(/\r\n/, $response_header_part);
     foreach my $response_header (@response_headers){
-		if($response_header =~ m/Transfer-Encoding/
-		   && $response_header =~ m/chunked/){
-			$chunked_flag = 1;
-			last;
-		}
+	if($response_header =~ m/Transfer-Encoding/
+	   && $response_header =~ m/chunked/){
+	    $chunked_flag = 1;
+	    last;
+	}
     }
 
     if($chunked_flag){
-		$response_body_part = unchunk($response_body_part);
+	$response_body_part = unchunk($response_body_part);
     }
 
     if($brief_flag){
-		return $response_body_part;
+	return $response_body_part;
     }else{
-		return $response;
+	return $response;
     }
 }
 
@@ -81,28 +81,28 @@ sub send_get{
     my $response = "";
 
     my $socket = new IO::Socket::INET(
-		PeerHost=>$host,
-		PeerPort=>$port,
-		Proto=>"tcp"
-		) or die "Error in socket creation: $!\n";
+	PeerHost=>$host,
+	PeerPort=>$port,
+	Proto=>"tcp"
+	) or die "Error in socket creation: $!\n";
 
     my %headermap = %{$ref_headermap};
     unless(defined($headermap{"Host"})){
-		$headermap{"Host"} = $host;
+	$headermap{"Host"} = sprintf("%s:%d", $host, $port);
     }
     unless(defined($headermap{"Connection"})){
-		$headermap{"Connection"} = "close";
+	$headermap{"Connection"} = "close";
     }
 
     print $socket "GET $req_uri HTTP/1.1\r\n";
 
     foreach my $headerkey (keys %headermap){
-		print $socket "$headerkey: $headermap{$headerkey}\r\n";
+	print $socket "$headerkey: $headermap{$headerkey}\r\n";
     }
     print $socket "\r\n";
 
     while(my $line=<$socket>){
-		$response = $response . $line;
+	$response = $response . $line;
     }
 
     shutdown($socket, 2);
@@ -114,21 +114,21 @@ sub send_get{
 
     my @response_headers = split(/\r\n/, $response_header_part);
     foreach my $response_header (@response_headers){
-		if($response_header =~ m/Transfer-Encoding/
-		   && $response_header =~ m/chunked/){
-			$chunked_flag = 1;
-			last;
-		}
+	if($response_header =~ m/Transfer-Encoding/
+	   && $response_header =~ m/chunked/){
+	    $chunked_flag = 1;
+	    last;
+	}
     }
 
     if($chunked_flag){
-		$response_body_part = unchunk($response_body_part);
+	$response_body_part = unchunk($response_body_part);
     }
 
     if($brief_flag){
-		return $response_body_part;
+	return $response_body_part;
     }else{
-		return $response;
+	return $response;
     }
 }
 
@@ -141,20 +141,20 @@ sub unchunk{
     my $chunk_size_expected = hex(shift(@lines));
     my $processed_chunk_size = 0;
     while(1){
-		my $current_line = shift(@lines);
-		unless(defined($current_line)){
-			last;
-		}
-		$unchunked = $unchunked . $current_line . "\n";
-		$processed_chunk_size += length($current_line);
+	my $current_line = shift(@lines);
+	unless(defined($current_line)){
+	    last;
+	}
+	$unchunked = $unchunked . $current_line . "\n";
+	$processed_chunk_size += length($current_line);
 
-		if($processed_chunk_size >= $chunk_size_expected){
-			$chunk_size_expected = hex(shift(@lines));
-			$processed_chunk_size = 0;
-		}
-		if($chunk_size_expected == 0){
-			last;
-		}
+	if($processed_chunk_size >= $chunk_size_expected){
+	    $chunk_size_expected = hex(shift(@lines));
+	    $processed_chunk_size = 0;
+	}
+	if($chunk_size_expected == 0){
+	    last;
+	}
     }
     return $unchunked;
 }
@@ -163,54 +163,54 @@ sub send_post{
     my ($class_or_self, $host, $port, $req_uri, $ref_parammap, $payload, $ref_headermap, $brief_flag) = @_;
     my $response = "";
 
-	my $request_line = "POST $req_uri HTTP/1.1\r\n";
+    my $request_line = "POST $req_uri HTTP/1.1\r\n";
     my %headermap = %{$ref_headermap};
     unless(defined($headermap{"Host"})){
-		$headermap{"Host"} = $host;
+	$headermap{"Host"} = sprintf("%s:%d", $host, $port);
     }
     unless(defined($headermap{"Connection"})){
-		$headermap{"Connection"} = "close";
+	$headermap{"Connection"} = "close";
     }
-	unless(defined($headermap{"Content-Type"})){
-		$headermap{"Content-Type"} = "application/x-www-form-urlencoded";
-	}
+    unless(defined($headermap{"Content-Type"})){
+	$headermap{"Content-Type"} = "application/x-www-form-urlencoded";
+    }
 
-	my $body_str = "";
-	my %parammap = %{$ref_parammap};
-	my $first_flag = 1;
-	foreach my $paramkey (keys %parammap){
-		my $paramvalue = $parammap{$paramkey};
-		if($first_flag){
-			$first_flag = 0;
-		}else{
-			$body_str .= "&";
-		}
-		my $param_pair = uri_escape($paramkey) . "=" . uri_escape($paramvalue);
-		$body_str .= $param_pair;
+    my $body_str = "";
+    my %parammap = %{$ref_parammap};
+    my $first_flag = 1;
+    foreach my $paramkey (keys %parammap){
+	my $paramvalue = $parammap{$paramkey};
+	if($first_flag){
+	    $first_flag = 0;
+	}else{
+	    $body_str .= "&";
 	}
-	if(defined($payload)){
-		$body_str = $payload;
-	}
-	my $body_size = length($body_str);
-	$headermap{"Content-Length"} = $body_size;
+	my $param_pair = uri_escape($paramkey) . "=" . uri_escape($paramvalue);
+	$body_str .= $param_pair;
+    }
+    if(defined($payload)){
+	$body_str = $payload;
+    }
+    my $body_size = length($body_str);
+    $headermap{"Content-Length"} = $body_size;
 
-	my $header_str = "";
+    my $header_str = "";
     foreach my $headerkey (keys %headermap){
-		$header_str .= "$headerkey: $headermap{$headerkey}\r\n";
+	$header_str .= "$headerkey: $headermap{$headerkey}\r\n";
     }
 
     my $socket = new IO::Socket::INET(
-		PeerHost=>$host,
-		PeerPort=>$port,
-		Proto=>"tcp"
-		) or die "Error in socket creation: $!\n";
-	print $socket $request_line;
-	print $socket $header_str;
+	PeerHost=>$host,
+	PeerPort=>$port,
+	Proto=>"tcp"
+	) or die "Error in socket creation: $!\n";
+    print $socket $request_line;
+    print $socket $header_str;
     print $socket "\r\n";
-	print $socket $body_str;
+    print $socket $body_str;
 
     while(my $line=<$socket>){
-		$response = $response . $line;
+	$response = $response . $line;
     }
 
     shutdown($socket, 2);
@@ -222,21 +222,21 @@ sub send_post{
 
     my @response_headers = split(/\r\n/, $response_header_part);
     foreach my $response_header (@response_headers){
-		if($response_header =~ m/Transfer-Encoding/
-		   && $response_header =~ m/chunked/){
-			$chunked_flag = 1;
-			last;
-		}
+	if($response_header =~ m/Transfer-Encoding/
+	   && $response_header =~ m/chunked/){
+	    $chunked_flag = 1;
+	    last;
+	}
     }
 
     if($chunked_flag){
-		$response_body_part = unchunk($response_body_part);
+	$response_body_part = unchunk($response_body_part);
     }
 
     if($brief_flag){
-		return $response_body_part;
+	return $response_body_part;
     }else{
-		return $response;
+	return $response;
     }
 }
 
