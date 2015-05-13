2ce43719229b4465e49aa4bee92a239c9e7e6bd1
heetak.choi <heetak.choi@campmobile.com>
2015-05-13 11:51:12 +0900

    유틸러티 함수 추가

--- perl/board/lib/Util.pm
+++ perl/board/lib/Util.pm
@@ -52,4 +52,26 @@ sub access_info{
     return ($self->{"access-id"}, $self->{"access-password"}, $self->{"auth-token"});
 }
 
+sub get_ymd{
+    my ($self, $day) = @_;
+    my $rightnow = time + (24*60*60)*$day;
+
+    my $ymd = sprintf "%04d%02d%02d"
+        , (localtime $rightnow) [5] + 1900
+        , (localtime $rightnow) [4] + 1
+        , (localtime $rightnow) [3];
+    return $ymd;
+}
+
+sub commify {
+	my ($self, $number) = @_;
+    my $text = reverse $number; #숫자를 역으로
+    $text =~ s/
+        (\d{3})  #3개의 연속된 숫자
+        (?=\d)   #그다음에 숫자가나오는(lookaheads)
+         (?!\d*\.)#소숫점 이하부문은 무시(lookbehinds)
+       /$1,/gx;
+    return scalar reverse $text;
+}
+
 return "Util.pm";
