9dd13f5a583bb0d82cfa3b9602c4cb8eae7e3b74
heetakchoi <heetakchoi@campmobile.com>
2015-06-15 23:30:48 +0900

    임의의 정수 갯수로 나누는 경우의 수

--- perl/travel.pl
+++ perl/travel.pl
@@ -3,33 +3,58 @@
 use strict;
 use warnings;
 
-sub add_children
+my $terminal = 6;
+my $index = 0;
+my @history_list = (Step->new(-1, ""));
 
-my ($start_value, $end_value) = (0, 5);
+while(1){
+	if($index >=$terminal){
+		last;
+	}
+	$index ++;
+	my @neo_history_list = ();
+	while(my $one_history = shift(@history_list)){
+		foreach ( (0..1) ){
+			my $step = Step->new($index, $one_history->history.$_);
+			push(@neo_history_list, $step);
+		}
+	}
+	@history_list = @neo_history_list;
+}
 
-my $root = Node->new(undef, $start_value);
-
+foreach my $one_step (@history_list){
+	my $raw_history = $one_step->history;
+	print $raw_history,  " - ";
+	foreach my $char_index ( (0..(length($raw_history)-1)) ){
+		if(substr($raw_history, $char_index, 1) > 0){
+			print ($char_index+1);
+			print " ";
+		}
+	}
+	print "\n";
+}
+print scalar(@history_list), "\n";
 
 {
-    package Node;
-    use strict;
-    use warnings;
-    sub new{
-	my ($class, $parent, $value) = @_;
-	$my $self = {};
-	$self->{"parent"} = $parent;
-	$self->{"value"} = $value;
-	bless($self, $class);
-	return $self;
-    }
-    sub parnet{
-	my ($self, $neo) = @_;
-	$self->{"parent"} = $neo if(defined($neo));
-	return $self->{"parent"};
-    }
-    sub value{
-	my ($self, $neo) = @_;
-	$self->{"value"} = $neo if(defined($neo));
-	return $self->{"value"};
-    }
+	package Step;
+	use strict;
+	use warnings;
+	sub new{
+		my ($class, $index, $history) = @_;
+		my $self = {};
+		$self->{"index"} = $index;
+		$self->{"history"} = $history;
+		bless($self, $class);
+		return $self;
+	}
+	sub index{
+		my ($self, $neo) = @_;
+		$self->{"index"} = $neo if(defined($neo));
+		return $self->{"index"};
+	}
+	sub history{
+		my ($self, $neo) = @_;
+		$self->{"history"} = $neo if(defined($neo));
+		return $self->{"history"};
+	}
 }
