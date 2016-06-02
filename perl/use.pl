b26fb81c4c7bc9b81faf5f19b6089fc612f178f9
endofhope <endofhope@naver.com>
2016-06-02 13:52:43 +0900

    수정

--- perl/use.pl
+++ perl/use.pl
@@ -14,20 +14,33 @@ my %headermap = ();
 my $rawdata = $pnet->send_get("endofhope.com", 80, "/php/instagram10.php", \%headermap, 1);
 my @lines = split( /\n/, $rawdata);
 
+my $data = "";
+my $flag = 0;
 foreach (@lines){
-    print "@@@@@@@@@@@@@@\n";
-    printf "\n%s\n", $_;
+    if(length($_)>0){
+	if($flag){
+	    if(length($_)<6){
+		next;
+	    }
+	    $data .= $_;
+	}
+    }else{
+	unless($flag){
+	    $flag = 1;
+	}
+    }
 }
+# print $data;
 
-my $json_txt = join("", @lines);
-
+my $json_txt = $data;
 my @pictures = ();
 my @captions = ();
 my @ymds = ();
-
-print $json_txt;
 
 $json->load_text($json_txt);
 my $init_node = $json->parse;
 my $next_url = $init_node->get("pagination")->get("next_url");
 my @data = $init_node->get("data")->array_gets();
+foreach (@data){
+    printf "%s\n", $_->get("images")->get("standard_resolution")->get("url")->get;
+}
