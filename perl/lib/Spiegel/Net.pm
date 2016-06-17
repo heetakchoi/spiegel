a45df4ab7834b4b7524071effaa736b9b9ead9a4
Heetak Choi <heetak.choi@campmobile.com>
2016-06-17 13:53:17 +0900

    수정

--- perl/lib/Spiegel/Net.pm
+++ perl/lib/Spiegel/Net.pm
@@ -9,7 +9,7 @@ use Mozilla::CA;
 use URI::Escape;
 
 use Exporter ("import");
-our @EXPORT_OK = ("send_get", "send_post", "send_ssl_get");
+our @EXPORT_OK = ("send_get", "send_post", "send_ssl_get", "send_ssl_post");
 
 $| = 1;
 
@@ -242,6 +242,106 @@ sub send_ssl_get{
     print $socket "\r\n";
     print "\r\n" if($debug_flag);
     print "== DEBUG INFO ==\n" if($debug_flag);
+
+    while(my $line=<$socket>){
+	$response = $response . $line;
+    }
+
+    shutdown($socket, 2);
+
+    my $chunked_flag = 0;
+    my $index_of_crlfcrlf = index($response, "\r\n\r\n");
+    my $response_header_part = substr($response, 0, $index_of_crlfcrlf);
+    my $response_body_part = substr($response, $index_of_crlfcrlf+4);
+
+    my @response_headers = split(/\r\n/, $response_header_part);
+    foreach my $response_header (@response_headers){
+	if($response_header =~ m/Transfer-Encoding/
+	   && $response_header =~ m/chunked/){
+	    $chunked_flag = 1;
+	    last;
+	}
+    }
+
+    if($chunked_flag){
+	$response_body_part = unchunk($response_body_part);
+    }
+
+    if($brief_flag){
+	return $response_body_part;
+    }else{
+	return $response;
+    }
+}
+
+sub send_ssl_post{
+    my ($host, $port, 
+	$req_uri, $ref_parammap, $payload, $ref_headermap, 
+	$brief_flag, $debug_flag) = @_;
+    my $response = "";
+    my $socket = IO::Socket::SSL->new(
+	PeerHost => "$host:$port",
+	SSL_verify_mode => 0x02,
+	SSL_ca_file => Mozilla::CA::SSL_ca_file(),
+	)
+	|| die "Can't connect: $@";
+
+    $socket->verify_hostname($host, "http")
+	|| die "hostname verification failure";
+    
+    my $request_line = "POST $req_uri HTTP/1.1\r\n";
+    my %headermap = %{$ref_headermap};
+    unless(defined($headermap{"Host"})){
+	if($port eq "80"){
+	    $headermap{"Host"} = sprintf("%s", $host);
+	}else{
+	    $headermap{"Host"} = sprintf("%s:%d", $host, $port);
+	}
+    }
+    unless(defined($headermap{"Connection"})){
+	$headermap{"Connection"} = "close";
+    }
+    unless(defined($headermap{"Content-Type"})){
+	$headermap{"Content-Type"} = "application/x-www-form-urlencoded";
+    }
+
+    my $body_str = "";
+    my %parammap = %{$ref_parammap};
+    my $first_flag = 1;
+    foreach my $paramkey (keys %parammap){
+	my $paramvalue = $parammap{$paramkey};
+	if($first_flag){
+	    $first_flag = 0;
+	}else{
+	    $body_str .= "&";
+	}
+	my $param_pair = uri_escape($paramkey) . "=" . uri_escape($paramvalue);
+	$body_str .= $param_pair;
+    }
+    if(defined($payload)){
+	$body_str = $payload;
+    }
+    my $body_size = length($body_str);
+    $headermap{"Content-Length"} = $body_size;
+
+    my $header_str = "";
+    foreach my $headerkey (keys %headermap){
+	$header_str .= "$headerkey: $headermap{$headerkey}\r\n";
+    }
+
+    print $socket $request_line;
+    print $socket $header_str;
+    print $socket "\r\n";
+    print $socket $body_str;
+
+    if($debug_flag){
+	print "== DEBUG INFO ==\n" if($debug_flag);
+	print $request_line;
+	print $header_str;
+	print "\r\n";
+	print $body_str;
+	print "\n== DEBUG INFO ==\n" if($debug_flag);
+    }
 
     while(my $line=<$socket>){
 	$response = $response . $line;
--- perl/test.pl
+++ perl/test.pl
@@ -5,7 +5,7 @@ use warnings;
 
 use lib "lib";
 use Spiegel::Util("trim", "get_ymd");
-use Spiegel::Net("send_get", "send_post", "send_ssl_get");
+use Spiegel::Net("send_get", "send_post", "send_ssl_get", "send_ssl_post");
 use Spiegel::Prime;
 
 sub test_util_trim;
@@ -16,6 +16,16 @@ sub test_net_send_post;
 sub test_net_send_ssl_get;
 
 sub test_prime;
+
+my %one_parammap = (
+    "q"=>"launcher",
+    "c"=>"apps",
+    );
+my $payload = undef;
+my %one_headermap = ();
+print send_ssl_post("play.google.com", 443,
+		    "/store/search", \%one_parammap, $payload, \%one_headermap,
+		    1, 1);
 
 # test_util_trim(" 안녕하세요 세계 ");
 # test_util_get_ymd(-1);
@@ -44,9 +54,7 @@ sub test_util_get_ymd{
     printf "today after %d day is %s\n", $arg, get_ymd($arg);
 }
 sub test_net_send_get{
-    my ($host, $port, 
-	$req_uri, $ref_headermap, 
-	$brief_flag, $debug_flag) = @_;
+    my ($host, $port, $req_uri, $ref_headermap,	$brief_flag, $debug_flag) = @_;
     print send_get($host, $port, 
 		   $req_uri, $ref_headermap, 
 		   $brief_flag, $debug_flag);
@@ -66,6 +74,14 @@ sub test_net_send_ssl_get{
     print send_ssl_get($host, $port, 
 		       $req_uri, $ref_headermap, 
 		       $brief_flag, $debug_flag);
+}
+sub test_net_send_ssl_post{
+    my ($host, $port, 
+	$req_uri, $ref_parammap, $payload, $ref_headermap, 
+	$brief_flag, $debug_flag) = @_;
+    print send_ssl_post($host, $port, 
+			$req_uri, $ref_parammap, $payload, $ref_headermap, 
+			$brief_flag, $debug_flag);
 }
 sub test_prime{
     my $prime = Spiegel::Prime->new("../data/primes.txt");
