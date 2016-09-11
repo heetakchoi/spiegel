901334b754dbc14a6db766fe5f97fbea370399b7
heetakchoi <heetakchoi@campmobile.com>
2016-09-11 11:14:39 +0900

    랭턴의 개미

--- perl/ant/ant.pl
+++ perl/ant/ant.pl
@@ -3,11 +3,15 @@
 use strict;
 use warnings;
 
+my $number = shift;
+$number = 10000 unless(defined($number));
+
 my %black_hash = ();
 
 my ($x, $y, $ori_pre, $color_pre, $ori_post, $color_post) = (0, 0, "N", "W");
-my $num = 0;
-while($num < 100000){
+my ($num, $max_x, $max_y, $min_x, $min_y) = (0, 0, 0, 0, 0);
+
+while($num < $number){
     $num ++;
     if($color_pre eq "W"){
 	$color_post = "B";
@@ -44,7 +48,6 @@ while($num < 100000){
     }elsif($color_post eq "B"){
 	$black_hash{$position} = 1;
     }
-    # printf "num % 8d (% 4d,% 4d) Opre %s Cpre %s Opost %s Cpost %s\n", $num, $x, $y, $ori_pre, $color_pre, $ori_post, $color_post;
 
     if($ori_post eq "N"){
 	$y += 1;
@@ -64,11 +67,42 @@ while($num < 100000){
     }else{
 	$color_pre = "W";
     }
+    $max_x = $x if($x > $max_x);
+    $max_y = $y if($y > $max_y);
+    $min_x = $x if($x < $min_x);
+    $min_y = $y if($y < $min_y);
 }
 
-open(my $fh_w, ">", "result.txt");
+open(my $fh_w, ">", "result.html");
+
+print $fh_w "<html>\n";
+print $fh_w "  <head>\n";
+print $fh_w "    <script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n";
+print $fh_w "    <script type=\"text/javascript\">\n";
+print $fh_w "      google.charts.load('current', {'packages':['corechart']});\n";
+print $fh_w "      google.charts.setOnLoadCallback(drawChart);\n";
+print $fh_w "      function drawChart() {\n";
+print $fh_w "        var data = google.visualization.arrayToDataTable([\n";
+print $fh_w "	       ['X', 'Ant position'],\n";
 foreach (keys %black_hash){
     my ($x, $y) = split /,/, $_;
-    printf $fh_w "%d,%d\n", $x, $y;
+    printf $fh_w "	       [%d,%d],\n", $x, $y;
 }
+print $fh_w "	     ]);\n";
+
+print $fh_w "	     var options = {\n";
+print $fh_w "          title: 'Ant',\n";
+printf $fh_w "         hAxis: {title: 'South/North', minValue: %d, maxValue: %d},\n", $min_x, $max_x;
+printf $fh_w "         vAxis: {title: 'West/East', minValue: %d, maxValue: %d},\n", $min_y, $max_y;
+print $fh_w "          legend: 'none'\n";
+print $fh_w "        };\n";
+print $fh_w "        var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));\n";
+print $fh_w "        chart.draw(data, options);\n";
+print $fh_w "      }\n";
+print $fh_w "    </script>\n";
+print $fh_w "  </head>\n";
+print $fh_w "  <body>\n";
+print $fh_w "    <div id=\"chart_div\" style=\"width: 900px; height: 500px;\"></div>\n";
+print $fh_w "  </body>\n";
+print $fh_w "</html>\n";
 close $fh_w;
